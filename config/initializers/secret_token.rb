a672c0b34bc9e80049086917200bdf4fddfb5872
Sharif Al-Hadidi <sharifhadidi@gmail.com>
2013-07-29 14:47:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerNews::Application.config.secret_token = '6d28dbc5e2259930d7e269bd5bca73f65058556750aa5780feddfb6d8dd84dc74da13699e4e3e74f23d6de578453ec8cacd15255a48105dd1f030c196981f9ac'
